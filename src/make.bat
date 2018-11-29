b6cb2e0b915dd43d5a2044a05d893ee1d4f7c295
Alexsandro-Vitor <avsc@cin.ufpe.br>
2018-11-29 12:52:28 -0200

    Fix executable name

--- src/make.bat
+++ src/make.bat
@@ -1,4 +1,4 @@
 set originalpath="%cd%"
 cd "%~dp0"
-g++ Main.cc -std=c++17 -o ..\bin\pmt.exe
+g++ Main.cc -std=c++17 -o ..\bin\ipmt.exe
 cd %originalpath%
--- src/make.sh
+++ src/make.sh
@@ -1,5 +1,5 @@
 thispath="$(dirname "$0")"
 cd "$thispath"
-g++ Main.cc -std=c++17 -o ../bin/pmt.exe
+g++ Main.cc -std=c++17 -o ../bin/ipmt.exe
 cd ~-
 echo "Compilação terminada"
