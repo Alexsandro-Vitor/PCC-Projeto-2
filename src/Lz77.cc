07b2b8df0da20a6c71b453ac8e8a6f4149f98290
Alexsandro Vítor <avsc@cin.ufpe.br>
2018-11-30 12:16:49 -0300

    Fix and test LZ77 encoding

--- src/Lz77.cc
+++ src/Lz77.cc
@@ -4,7 +4,7 @@ void build_fsm(string pat, string ab, vector< unordered_map<char, int> >& delta)
 		delta[0][c] = 0;
 	delta[0][pat[0]] = 1;
 	int brd = 0;
-	for (int i = 1; i <= pat.size(); i++) {
+	for (unsigned int i = 1; i <= pat.size(); i++) {
 		for (const char a : ab)
 			delta[i][a] = delta[brd][a];
 		delta[i][pat[i]] = i + 1;
@@ -17,12 +17,12 @@ void build_fsm(string pat, string ab, vector< unordered_map<char, int> >& delta)
 //Prints the fsm
 void print_fsm(string pat, string ab, vector< unordered_map<char, int> >& delta) {
 	cout << "  ";
-	for (int i = 0; i < ab.size(); i++) {
+	for (unsigned int i = 0; i < ab.size(); i++) {
 		cout << ab[i] << ' ';
 	} cout << endl;
-	for (int i = 0; i < pat.size(); i++) {
+	for (unsigned int i = 0; i < pat.size(); i++) {
 		cout << pat[i] << ' ';
-		for (int j = 0; j < ab.size(); j++) {
+		for (unsigned int j = 0; j < ab.size(); j++) {
 			cout << delta[i][ab[j]] << ' ';
 		} cout << endl;
 	}
@@ -33,7 +33,7 @@ void prefix_match(string window, string pat, string ab, unsigned int& pos, unsig
 	build_fsm(pat, ab, fsm);
 	pos = maxlen = 0;
 	unsigned int cur = 0, ls = window.size() - pat.size();
-	for (int i = 0; i < window.size();) {
+	for (unsigned int i = 0; i < window.size();) {
 		cur = fsm[cur][window[i]];
 		if (++i - cur < ls && cur > maxlen) {
 			maxlen = cur;
@@ -45,8 +45,9 @@ void prefix_match(string window, string pat, string ab, unsigned int& pos, unsig
 
 void int_encode(unsigned int x, string ab, string& code) {
 	code.clear();
+	if (!x) code = ab[0];
 	while (x) {
-		int bit = x % ab.size();
+		unsigned int bit = x % ab.size();
 		code = ab[bit] + code;
 		x /= ab.size();
 	}
@@ -56,13 +57,21 @@ void lz77_encode(string txt, unsigned int ls, unsigned int ll, string ab, string
 	string W = string(ls, ab[0]) + txt;
 	code.clear();
 	unsigned int p, l;
-	for (unsigned int j = ls; j < W.size() * ls; j += l + 1) {
+	for (unsigned int j = ls; j < W.size(); j += l + 1) {
 		unsigned int tempindex = min(W.size(), j+ll);
+		cout << "window: " << W.substr(j-ls, tempindex - (j-ls)) << endl;
+		cout << "pat: " << W.substr(j, tempindex - j) << endl;
 		prefix_match(W.substr(j-ls, tempindex - (j-ls)), W.substr(j, tempindex - j), ab, p, l);
 		string tempcode;
 		int_encode(p, ab, tempcode);
+		cout << "p = " << p << endl;
+		cout << "int_encode(p) = " << tempcode << endl;
 		code += tempcode;
 		int_encode(l, ab, tempcode);
-		code += W[j+1];
+		cout << "l = " << l << endl;
+		cout << "int_encode(l) = " << tempcode << endl;
+		code += tempcode;
+		cout << "W[j+l] = " << W[j+l] << endl;
+		code += W[j+l];
 	}
 }
--- src/Main.cc
+++ src/Main.cc
@@ -11,14 +11,14 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 	cout << "prefix_match test" << endl;
-	string pattern = "test";
-	string alphabet = "aeiostu";
+	string pattern = "testando";
+	string alphabet = "0123456789abcdefghijklmnopqrstuvwxyz";
 	unsigned int pos, maxlen;
 	prefix_match("aatesta", pattern, alphabet, pos, maxlen);
 	cout << "pos: " << pos << endl;
 	cout << "maxlen: " << maxlen << endl;
 
-	cout << "int_encode test" << endl;
+	cout << endl << "int_encode test" << endl;
 	unsigned int x = 194;
 	string code;
 	int_encode(x, "01", code);
@@ -27,5 +27,9 @@ int main(int argc, char* argv[]) {
 	cout << "oct: " << code << endl;
 	int_encode(x, "0123456789ABCDEF", code);
 	cout << "hex: " << code << endl;
+
+	cout << endl << "l77 test" << endl;
+	lz77_encode("abracadabra pe de cabra", 7, 5, alphabet, code);
+	cout << "Code: " << code << endl;
 	return 0;
 }
